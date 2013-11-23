c26ea9af8d2e9abc78b42c0f65f21ec316bf8ec4
Paul Wise <pabs3@bonedaddy.net>
2013-11-23 17:57:36 +0800

    sysctl takes a pointer to size_t not int.

--- src/procinfo.c
+++ src/procinfo.c
@@ -63,7 +63,7 @@ void get_info(int pid, struct procinfo *p)
 int fill_kinfo(struct kinfo_proc *info, int pid)
 {
 	int mib[] = { CTL_KERN, KERN_PROC, KERN_PROC_PID, pid };
-	int len = sizeof *info;
+	size_t len = sizeof *info;
 	if(sysctl(mib, 4, info, &len, 0, 0) == -1) 
 		return -1;
 	return len?0:-1;
@@ -122,7 +122,8 @@ int get_term(char *tty)
 int get_login_pid(char *tty)
 {
 	int mib[4] = {CTL_KERN, KERN_PROC, KERN_PROC_TTY, 0};
-	int len, t, el, i, pid, cndt = -1, l;
+	int t, el, i, pid, cndt = -1, l;
+	size_t len;
 	struct kinfo_proc *info;
 	struct procinfo p;
 	
@@ -167,7 +168,8 @@ int get_login_pid(char *tty)
 int get_all_info(struct kinfo_proc **info)
 {
 	int mib[3] = { CTL_KERN, KERN_PROC, KERN_PROC_ALL };
-	int len, el;
+	int el;
+	size_t len;
 
 	if(sysctl(mib, 3, 0, &len, 0, 0) == -1)
 		return 0;
